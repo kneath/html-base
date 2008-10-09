2d77676470c7f3d3d3f3cdee5284f14f4515be45
Kyle Neath <kneath@gmail.com>
2008-10-09 02:08:58 -0700

    InsertPartial spec helper: remove htmlContainer if it exists first

--- js/spec/helpers/insert_partial.js
+++ js/spec/helpers/insert_partial.js
@@ -1,4 +1,5 @@
 var insertPartial = function(url){
+  if ($('htmlContainer') != null) $('htmlContainer').dispose();
   var container = new Element('div', {id: 'htmlContainer', style:'display:none'});
   document.body.appendChild(container);
   new Request.HTML({url: '/js/spec/specs/' + url, update:'htmlContainer', async: false}).send();
